912274a0e42b3c5f4f18d240ba369724c4c739ba
Maxime-Gld <Maxime.Gdev@hotmail.com>
2022-12-16 14:39:08 +0100

    affichage des produits avec des createElement

--- front/js/script.js
+++ front/js/script.js
@@ -1,36 +1,52 @@
 // affichage des produits dans la page d'acceuil
-    // 1ere etape - On recupere les données depuis l'API
-    // 2eme etape - on stock les produits dans une variable
-    // 3eme etape - on insere la variable dans le HTML
-        // 3.1 - on cible l'id dans le HTML
-        // 3.2 - on modifie le code en le remplacant par la variable
 
-// 1ere etape
+// fonction permettant de générer le HTML pour afficher un produit
+function createElt(item) {
+    let link = document.createElement("a");
+        link.setAttribute("href", `./product.html?id=${item._id}`);
+        document.querySelector("#items").prepend(link);
+
+    let article = document.createElement("article");
+        document.querySelector("#items a").append(article);
+
+    let img = document.createElement("img");
+        img.setAttribute("src", item.imageUrl);
+        img.setAttribute("alt", item.altTxt);
+        document.querySelector("#items article").append(img);
+
+    let title = document.createElement("h3");
+        title.classList.add("productName");
+        title.textContent = item.name;
+        document.querySelector("#items article").append(title);
+
+
+    let description = document.createElement("p");
+        description.classList.add("productDescription");
+        description.textContent = item.description;
+        document.querySelector("#items article").append(description);
+}
+
+
+
+
+// on recupère les données dans l'API avec un fetch et on verifie que tout se passe bien
 fetch("http://localhost:3000/api/products/")
     .then(function(res) {
         if (res.ok) { 
             return res.json();
         }
     })
-// 2eme etape
+
+
+// si le fetch c'est bien passé on appel la fonction pour afficher chaque produit sur la page d'acceuil
     .then(function(value) {
-        let allItems = ""
-        for (let item of value) { 
-            allItems += `
-                <a href="./product.html?id=${item._id}">
-                    <article>
-                    <img src="${item.imageUrl}" alt="${item.altTxt}">
-                    <h3 class="productName">${item.name}</h3>
-                    <p class="productDescription">${item.description}</p>
-                    </article>
-                </a>`
-            }
-// 3eme etape
-        return document
-                .getElementById('items')
-                .innerHTML = allItems;
+        for (let item of value) {
+            createElt(item);
+        }
     })
-// Si il y a une erreur de recupération des produits
+
+
+// Si il y a une erreur de recupération des produits on affiche un message
     .catch(function(err) {
         alert("Une erreur est survenue : \n .catch : " + err);
     })
